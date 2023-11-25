b358df996c5596a6c348ce5a559e72daac9e2baf
codeincluded <codeincluded@gmail.com>
2023-11-25 11:52:45 +0200

    nvimtree smart toggle

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -20,16 +20,16 @@ api.events.subscribe(api.events.Event.FileCreated, function(file)
     vim.cmd("edit " .. file.fname)
 end)
 
--- toggle the window
-local nvimTreeFocusOrToggle = function()
-    local nvimTree = require("nvim-tree.api")
-    local currentBuf = vim.api.nvim_get_current_buf()
-    local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
-    if currentBufFt == "NvimTree" then
-        nvimTree.tree.toggle()
-    else
-        nvimTree.tree.focus()
-    end
+local nvimTreeFocusOrToggle = function ()
+	local nvimTree=require("nvim-tree.api")
+	local currentBuf = vim.api.nvim_get_current_buf()
+	local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
+	if currentBufFt == "NvimTree" then
+		nvimTree.tree.toggle()
+	else
+		nvimTree.tree.focus()
+	end
 end
 
-vim.keymap.set("n", "<leader>pv", nvimTreeFocusOrToggle)
+vim.keymap.set("n", "<A-1>", nvimTreeFocusOrToggle)
+
